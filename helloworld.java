4ee4c2960de9e1221861da53b0d906fa2bf30885
Sean Fullmer <sean.fullmer@tooeleschools.org>
2020-09-14 09:54:31 -0600

    9/14/2020

--- helloworld.java
+++ helloworld.java
@@ -2,14 +2,14 @@ class helloworld{
 
 	public static void main(String[] args){
 		System.out.print("hello world");
-System.out.print("phone number 2084066801");
-System.out.print("b1 math");
-System.out.print("b2 us history");
-System.out.print("b3 release time");
-System.out.print("b4 b5 python");
-System.out.print("s6 s7 java");
-System.out.print("s8 astrnomy");
-System.out.print("s9 la 11");
-System.out.print("s10 fit for life");
+System.out.print(" phone number 2084066801");
+System.out.print(" b1 math");
+System.out.print(" b2 us history");
+System.out.print(" b3 release time");
+System.out.print(" b4 b5 python");
+System.out.print(" s6 s7 java");
+System.out.print(" s8 astrnomy");
+System.out.print(" s9 la 11");
+System.out.print(" s10 fit for life");
 	}
 }
---
+++ i fell asleep.java
@@ -0,0 +1,9 @@
+class Ch2exe{
+
+
+    //main entry point
+    public static void main (String[] args){
+        String name = "sean fullmer"
+                
+    }
+}
